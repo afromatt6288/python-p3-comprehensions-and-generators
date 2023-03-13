28a851728da78375806f55b53ca2a08af0c53103
Matthew Clark <afromatt6288@hotmail.com>
2023-03-13 10:46:36 -0600

    Lab Complete

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    evens = [n for n in num_list if n % 2 == 0]
+    return(evens)
     pass
 
 def make_exclamation(sentence_list):
+    return [f'{n}!' for n in sentence_list]
+    # return [(n, end="!") for n in sentence_list]
     pass
